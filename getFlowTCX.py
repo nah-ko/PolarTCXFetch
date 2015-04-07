761a87ca69c3c3b9f9dade4891202f6af1f86381
nah-ko <christophe.truffier+github@gmail.com>
2015-04-07 19:14:49 +0200

    Polar Flow webservice send zip files, dealing with; adding one more parameter for sync days

--- getFlowTCX.py
+++ getFlowTCX.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
-import datetime
+from datetime import datetime,  timedelta
 import requests
 import json
+import zipfile
+import StringIO
 
 with open("settings.json", "r") as f:
     settings = json.load(f)
 
-startDate = "01.03.2015"
-endDate  = "01.04.2015"
-loginURL = "https://flow.polar.com/login"
+# Start from 'sync_days' before today
+today = datetime.now()
+firstDate = today - timedelta(days = settings['sync_days'])
+startDate = firstDate.strftime("%d.%m.%Y")
+endDate  = today.strftime("%d.%m.%Y")
+# Give website a User-Agent who won't be rejected...
 myHeaders = {'user-agent':  'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
 
-login = requests.post(loginURL, 
+login = requests.post("https://flow.polar.com/login", 
                       data={"email":settings['polarflow_email'],"password":settings['polarflow_pass']}, 
                       headers=myHeaders)
 
@@ -21,11 +26,14 @@ activities = requests.get("https://flow.polar.com/training/getCalendarEvents?sta
 #Get information only from 'EXERCISE' type activities found by:   activities.json[x]['type']
 for activity in activities.json:
     if activity['type'] == 'EXERCISE':
-        print("Fetch activity from: %s (ID: %s), TCX Url: https://flow.polar.com/%s/export/tcx" % (activity['datetime'],  activity['listItemId'],  activity['url']))
-        tcxFile = requests.get("https://flow.polar.com/%s/export/tcx" % activity['url'], 
+        print("Fetch activity from: %s (ID: %s), TCX Url: https://flow.polar.com%s/export/tcx" % (activity['datetime'],  activity['listItemId'],  activity['url']))
+        tcxFile = requests.get("https://flow.polar.com%s/export/tcx" % activity['url'], 
                                cookies=login.cookies
         )
-        filename = "%s_%s.tcx" % (activity['listItemId'],  activity['datetime'].split('T')[0])
-        # Save the file locally, just in case.
-        with file("archives/" + filename, "w") as f:
-            f.write(tcxFile.text.encode("UTF-8"))
+        # Polar Flow exports activities in zipfiles, so we need to get it and then extract all files.
+        try:
+            zipDoc = zipfile.ZipFile(StringIO.StringIO(tcxFile.content))
+        except:
+            print("Error extracting file: not a zip file")
+            continue
+        zipDoc.extractall("archives/")
