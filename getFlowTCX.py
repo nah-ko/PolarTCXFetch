52a8ead05007f4bcaf88ce5a375cffb0021a81b3
nah-ko <christophe.truffier+github@gmail.com>
2015-04-19 14:25:25 +0200

    Store last time we fetch TCX files

--- getFlowTCX.py
+++ getFlowTCX.py
@@ -8,11 +8,23 @@ import StringIO
 with open("settings.json", "r") as f:
     settings = json.load(f)
 
-# Start from 'sync_days' before today
+# Start from last time synchronization was done (with lastsync.dat file),
+# if file doesn't exists, start from 'sync_days' before today
 today = datetime.now()
-firstDate = today - timedelta(days = settings['sync_days'])
+try:
+    fd = open("lastsync.dat",  "r")
+    D = fd.read()
+    fd.close()
+    firstDate = datetime.strptime(D,  "%d.%m.%Y")
+except:
+    print("Problem reading lastsync.dat")
+    firstDate = today - timedelta(days = settings['sync_days'])
 startDate = firstDate.strftime("%d.%m.%Y")
 endDate  = today.strftime("%d.%m.%Y")
+# Write last time we synchronize into lastsync.dat
+fd = open("lastsync.dat",  "w")
+fd.write(endDate)
+fd.close()
 # Give website a User-Agent who won't be rejected...
 myHeaders = {'user-agent':  'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
 
