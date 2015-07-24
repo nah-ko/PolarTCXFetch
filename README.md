03a670310372d1fe5e6b6feeb6c1475f7b15062b
nah-ko <christophe.truffier+github@gmail.com>
2015-07-24 17:39:39 +0200

    Some improvements: more information given during process, managing directories and sports type, etc

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ This script intend to download training file from Polar Flow (http://flow.polar.
 ## Instructions
 1. Copy settings-sample.json to settings.json and enter your Polar Flow credentials;
 2. Enter the synchronisation days back in the past;
-3. Create archives directory where TCX files will be stored;
+3. Give location of the archives directory where TCX files will be stored;
 4. Run the script.
+
+## Description
+This script log into Flow web application, request for your activities (depending on last day you synchronise your files) and download them as training files (aka TCX files).
+If the directory needed doesn't exists, it'll be created for you.
 
--- getFlowTCX.py
+++ getFlowTCX.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
+import requests,  json,  zipfile,  StringIO,  os,  shutil
 from datetime import datetime,  timedelta
-import requests
-import json
-import zipfile
-import StringIO
-import os
 from lxml import objectify
 
 with open("settings.json", "r") as f:
@@ -19,7 +15,7 @@ try:
     fd.close()
     firstDate = datetime.strptime(D,  "%d.%m.%Y")
 except:
-    print("Problem reading lastsync.dat")
+    print("Problem reading lastsync.dat (file missing or unreadable)")
     firstDate = today - timedelta(days = settings['sync_days'])
 startDate = firstDate.strftime("%d.%m.%Y")
 endDate  = today.strftime("%d.%m.%Y")
@@ -30,23 +26,32 @@ fd.close()
 # Give website a User-Agent who won't be rejected...
 myHeaders = {'user-agent':  'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
 
+print("Connecting to Polar Flow webapp...")
+
 login = requests.post("https://flow.polar.com/login", 
                       data={"email":settings['polarflow_email'],"password":settings['polarflow_pass']}, 
                       headers=myHeaders)
+login.raise_for_status()
+#if login.raise_for_status():
+#    print("Ooopss!! Something went wrong...\nError is: {}".format(login.raise_for_status()))
+#    exit(1)
+
+print("Connected!\nRequesting last unsync activities since {}...".format(startDate))
 
 activities = requests.get("https://flow.polar.com/training/getCalendarEvents?start=%s&end=%s" % (startDate,  endDate), 
                           cookies=login.cookies  )
 
 #Get information only from 'EXERCISE' type activities found by:   activities.json[x]['type']
 # 4 Jul 2015: activities.json no longer work (no reason)
+print("Fetch {} activities.".format(activities.content.count('EXERCISE')))
 for activity in json.loads(activities.content):
     if activity['type'] == 'EXERCISE':
-        print("Fetch activity from: %s (ID: %s), TCX Url: https://flow.polar.com%s/export/tcx/true" % (activity['datetime'],  activity['listItemId'],  activity['url']))
+        print("Fetch activity from: %s (ID: %s);\n\tTCX Url: https://flow.polar.com%s/export/tcx/true" % (activity['datetime'],  activity['listItemId'],  activity['url']))
         tcxFile = requests.get("https://flow.polar.com%s/export/tcx/true" % activity['url'], 
                                cookies=login.cookies
         )
         # Make sure there is a directory where to download files, if not we'll create it
-        archivesDir = "archives/"
+        archivesDir = settings['archives_dir']
         if not os.path.exists(archivesDir):
             os.mkdir(archivesDir)
         # Polar Flow exports activities in zipfiles, so we need to get it and then extract all files.
@@ -66,4 +71,15 @@ for activity in json.loads(activities.content):
         if ActivityType == 'Other':
             tcxFileName_Right = tcxFileName.split('_')[1]
             ActivityType = str(tcxFileName_Right.split('.')[0])
-        print("Activity (type: {}) saved to: {}/{}".format(ActivityType,  archivesDir,  tcxFileName))
+        # Move file to the associated sport type directory
+        sourceFile = archivesDir + tcxFileName
+        destDir = archivesDir + ActivityType + '/'
+        try:
+            if not os.path.exists(destDir):
+                os.mkdir(destDir)
+            shutil.move(sourceFile, destDir)
+            print("Activity (type: {}) saved to: {}/{}".format(ActivityType,  destDir,  tcxFileName))
+        except (shutil.Error,  IOError, os.error), why:
+            print("Unable to move {} to {} because of error: {}".format(sourceFile, destDir, str(why)))
+
+print("Every training file is yours !")
